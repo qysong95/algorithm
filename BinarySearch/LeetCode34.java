f466e870142be1b26b5aab47b0473d5171a2dde4
QiuyuanSong <31904169+qysong95@users.noreply.github.com>
2019-01-08 03:01:44 -0500

    Find First and Last Position of Element in Sorted Array

---
+++ BinarySearch/LeetCode34.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[]{-1, -1};
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (nums[left] != target) {
+            return result;
+        } else {
+            result[0] = left;
+        }
+        right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (nums[right] == target) {
+            result[1] = right;
+        } else {
+            result[1] = left;
+        }
+        return result;
+    }
+}
