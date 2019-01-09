2dc63a55579be72f16bcd65ed2794a582c9e9522
QiuyuanSong <31904169+qysong95@users.noreply.github.com>
2019-01-09 14:12:29 -0500

    Search Insert Position

---
+++ BinarySearch/LeetCode35.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (target <= nums[left]) {
+            return left;
+        } else if (target > nums[right]) {
+            return right + 1;
+        } else {
+            return right;
+        }
+    }
+}
