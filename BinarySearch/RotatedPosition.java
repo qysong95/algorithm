4a121f30e99e2ec352f9a465d0072e9b7d7af0ce
QiuyuanSong <31904169+qysong95@users.noreply.github.com>
2019-01-10 15:35:41 -0500

    Update RotatedPosition.java

--- BinarySearch/RotatedPosition.java
+++ BinarySearch/RotatedPosition.java
@@ -5,12 +5,16 @@ public int rotatedPosition(int[] array) {
     }
     int left = 0;
     int right = array.length - 1;
-    while (left < right - 1) {
+    // check if the array is in order
+    if (array[left] <= array[right]) {
+        return 0;
+    }
+    while (left < right - 1) {  // we want to terminate if left neighbors right
         int mid = left + (right - left) / 2;
         if (array[mid] > array[left]) {
-            left = mid + 1;
+            left = mid; // left = mid + 1 (wrong!!! when input.length == 3, it would be left == right after first round
         } else {
-            right = mid - 1;
+            right = mid; // right = mid - 1 (wrong!!!)
         }
     }
     if (array[left] <= array[right]) {
