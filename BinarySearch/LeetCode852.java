00286955c68a0a92cbfc58ea0f2d3b7d438f02e7
QiuyuanSong <31904169+qysong95@users.noreply.github.com>
2019-01-10 15:48:18 -0500

    Peak Index in a Mountain Array

---
+++ BinarySearch/LeetCode852.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int left = 0;
+        int right = A.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
+                return mid;
+            } else if (A[mid - 1] < A[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
