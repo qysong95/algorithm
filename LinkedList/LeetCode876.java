28c7ab82ae4f06264040e22a11b5a719dc13310f
QiuyuanSong <31904169+qysong95@users.noreply.github.com>
2019-01-20 20:03:08 -0500

    Middle of the Linked List

---
+++ LinkedList/LeetCode876.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return fast.next == null ? slow : slow.next;
+    }
+}
+/*
+two pointers
+    - fast: fast = fast.next.next
+    - slow: slow = slow.next
+*/
