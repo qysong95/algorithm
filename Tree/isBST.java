5e8b1c9cdb3d63f80dbfcdf23a014012cbb6ad8f
QiuyuanSong <31904169+qysong95@users.noreply.github.com>
2019-01-23 14:25:02 -0500

    Check if a tree is binary search tree

---
+++ Tree/isBST.java
@@ -0,0 +1,16 @@
+public boolean isBST(TreeNode root) {
+    if (root == null) {
+        return true;
+    }
+    return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+}
+
+private boolean helper(TreeNode root, int min, int max) {
+    if (root == null) {
+        return true;
+    }
+    if (root.val <= min || root.val >= max) {
+        return false;
+    }
+    return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+}
